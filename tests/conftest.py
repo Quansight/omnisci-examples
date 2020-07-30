d49ef120b933a6639baebecd68a88480b1ceb274
Kim Pevey <kcpevey@gmail.com>
2020-07-30 14:14:39 -0500

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,13 @@ OMNISCIDB_USER = 'admin'
 OMNISCIDB_PASSWORD = 'HyperInteractive'
 OMNISCIDB_PROTOCOL = 'binary'
 
+EXT_OMNISCIDB_HOST = 'metis.mapd.com'
+EXT_OMNISCIDB_PORT = 443
+EXT_OMNISCIDB_DATABASE = 'mapd'
+EXT_OMNISCIDB_USER = 'demouser'
+EXT_OMNISCIDB_PASSWORD = 'HyperInteractive'
+EXT_OMNISCIDB_PROTOCOL = 'https'
+
 
 @pytest.fixture(scope='module')
 def con():
@@ -24,5 +31,22 @@ def con():
         host=OMNISCIDB_HOST,
         port=OMNISCIDB_PORT,
         database=OMNISCIDB_DATABASE
+    )
+    return omnisci_client
+
+
+@pytest.fixture(scope='module')
+def con_external():
+    """Define a connection fixture.
+    Returns
+    -------
+    ibis.omniscidb.OmniSciDBClient
+    """
+    omnisci_client = ibis.omniscidb.connect(
+        user=EXT_OMNISCIDB_USER,
+        password=EXT_OMNISCIDB_PASSWORD,
+        host=EXT_OMNISCIDB_HOST,
+        port=EXT_OMNISCIDB_PORT,
+        database=EXT_OMNISCIDB_DATABASE
     )
     return omnisci_client
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,19 @@
 
-def test_db_connection(con):
+def test_db_connection_local(con):
     assert con.user == 'admin'
+
+
+def test_db_connection_external(con_external):
+    """Test the connection to the external database"""
+    assert con_external.user == 'demouser'
+
+
+def test_db_table_exists(con_external):
+    """test the external db table exists
+    For the ibis-vega-transform notebooks
+    """
+    # get the list of tables in the external db
+    db_table_names = con_external.list_tables()
+    # ensure the table we need is included
+    assert "flights_donotmodify" in db_table_names
+    assert "tweets_nov_feb" in db_table_names

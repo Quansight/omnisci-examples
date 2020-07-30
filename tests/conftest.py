dec6d301e7cca79aeb7650ed68dc3f504eea57b0
Kim Pevey <kcpevey@quansight.com>
2020-07-30 15:07:33 -0500

    add notebooks (#2)
    * downgrade to omnisci 5.2.2
    
    * add notebooks
    
    * add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 """OmniSciDB test configuration module."""
-import pytest
-
 import ibis
+import pytest
 
 OMNISCIDB_HOST = '127.0.0.1'
 OMNISCIDB_PORT = 6274
@@ -9,6 +8,13 @@ OMNISCIDB_DATABASE = 'omnisci'
 OMNISCIDB_USER = 'admin'
 OMNISCIDB_PASSWORD = 'HyperInteractive'
 OMNISCIDB_PROTOCOL = 'binary'
+
+EXT_OMNISCIDB_HOST = 'metis.mapd.com'
+EXT_OMNISCIDB_PORT = 443
+EXT_OMNISCIDB_DATABASE = 'mapd'
+EXT_OMNISCIDB_USER = 'demouser'
+EXT_OMNISCIDB_PASSWORD = 'HyperInteractive'
+EXT_OMNISCIDB_PROTOCOL = 'https'
 
 
 @pytest.fixture(scope='module')
@@ -23,6 +29,25 @@ def con():
         password=OMNISCIDB_PASSWORD,
         host=OMNISCIDB_HOST,
         port=OMNISCIDB_PORT,
-        database=OMNISCIDB_DATABASE
+        database=OMNISCIDB_DATABASE,
+        protocol=OMNISCIDB_PROTOCOL
+    )
+    return omnisci_client
+
+
+@pytest.fixture(scope='module')
+def con_external():
+    """Define a connection fixture.
+    Returns
+    -------
+    ibis.omniscidb.OmniSciDBClient
+    """
+    omnisci_client = ibis.omniscidb.connect(
+        user=EXT_OMNISCIDB_USER,
+        password=EXT_OMNISCIDB_PASSWORD,
+        host=EXT_OMNISCIDB_HOST,
+        port=EXT_OMNISCIDB_PORT,
+        database=EXT_OMNISCIDB_DATABASE,
+        protocol=EXT_OMNISCIDB_PROTOCOL
     )
     return omnisci_client
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,13 @@
-
-def test_db_connection(con):
+def test_db_connection_local(con):
     assert con.user == 'admin'
+
+
+def test_db_table_exists(con_external):
+    """test the external db table exists
+    For the ibis-vega-transform notebooks
+    """
+    # get the list of tables in the external db
+    db_table_names = con_external.list_tables()
+    # ensure the table we need is included
+    assert "flights_donotmodify" in db_table_names
+    assert "tweets_nov_feb" in db_table_names

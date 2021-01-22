f3fb4c845a665ff929e2f1f3a361161e87dca014
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-01-22 12:48:14 -0400

    Change the conda target directory

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-NOTEBOOK_DIR=${PREFIX}/opt/omnisci/
-mkdir -p ${NOTEBOOK_DIR}
+TARGET_DIR=${PREFIX}/opt/omnisci-examples/
+mkdir -p ${TARGET_DIR}
 
-cp -R notebooks ${NOTEBOOK_DIR}
+# remore .git if exists
+rm -rf .git
+
+cp -R . ${TARGET_DIR}

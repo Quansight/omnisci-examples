a01aefc1bd261ec7c65ad6be91aff01f4d621310
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-01-22 13:07:14 -0400

    Fix conda packaging build script

--- conda/build.sh
+++ conda/build.sh
@@ -3,7 +3,7 @@
 TARGET_DIR=${PREFIX}/opt/omnisci-examples/
 mkdir -p ${TARGET_DIR}
 
-# remore .git if exists
-rm -rf .git
-
 cp -R . ${TARGET_DIR}
+
+# remore .git if exists
+rm -rf ${TARGET_DIR}/.git
